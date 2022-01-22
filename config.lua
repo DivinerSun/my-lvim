00c5fd96415cfb3411c83d906a74ada74c375a8c
Diviner <sqq_job@sina.com>
2022-01-22 22:54:28 +0800

    feat: 更新

--- config.lua
+++ config.lua
@@ -34,7 +34,7 @@ local linters = require("lvim.lsp.null-ls.linters")
 linters.setup({
 	{
 		exe = "eslint_d",
-		filetypes = { "vue", "html", "css", "less", "scss" },
+		filetypes = { "vue", "css", "less", "scss" },
 	},
 })
 
